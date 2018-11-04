2f9f2ca84d699b9a8379dd939c95877287660332
Kade <keithkade@gmail.com>
2018-11-04 10:12:42 -0600

    faster graph folding

--- scripts/fold_graph.py
+++ scripts/fold_graph.py
@@ -14,18 +14,21 @@ graph_to_fold = 'graphs/bipartite_connected_by_comment.graph'
 folded_grph_str = 'graphs/bipartite_connected_by_comment_folded.graph'
 
 FIn = snap.TFIn(graph_to_fold)
-folded_graph = snap.TUNGraph.Load(FIn)
+graph_to_fold = snap.TUNGraph.Load(FIn)
 
 node_id_to_info = {}
+info_to_node_id = {}
 
 subreddit_count = len(subreddits)
 user_count      = len(users)
 
 for subreddit in subreddits:
-    node_id_to_info[subreddit.Index] = { 'type': 'subreddit', 'id': subreddit.base36_id }
+    node_id_to_info[subreddit.Index] = { 'type': 'subreddit', 'id': subreddit.base36_id, 'name': subreddit.name }
+    info_to_node_id[subreddit.base36_id] = subreddit.Index
 
 for user in users:
     node_id_to_info[user.Index + subreddit_count] = { 'type': 'user', 'id': user.name }
+    info_to_node_id[user.name] = user.Index + subreddit_count
 
 def fold_graph():
   print 'Folding graph'
@@ -38,26 +41,41 @@ def fold_graph():
       folded_graph.AddNode(key)
       nodes_list.append(key)
 
-  # get all possible pairs of subreddits
-  comb = combinations(nodes_list, 2)
-
-  print str(len(list(comb))) + ' combinations to check'
-
-  for pair in list(comb):
-    Nbrs = snap.TIntV()
-    snap.GetCmnNbrs(graph, pair[0], pair[1], Nbrs)
-    # if the two subreddits have a user in common, connect them
-    if Nbrs.Len() > 0:
-      folded_graph.AddEdge(pair[0], pair[1])
+  # connect all subreddits where the same user commented in both
+  for user in users:
+      user_node = graph_to_fold.GetNI(info_to_node_id[user.name])
+      neighbors = []
+      for neighbor_id in user_node.GetOutEdges():
+          neighbors.append(neighbor_id)
+      comb = combinations(neighbors, 2)
+      for pair in list(comb):
+          folded_graph.AddEdge(pair[0], pair[1])
 
   print 'Saving graph'
   FOut = snap.TFOut(folded_grph_str)
   folded_graph.Save(FOut)
   FOut.Flush()
 
-  return folded_graph
+def get_jaccard(g, node_id):
+  target = g.GetNI(node_id)
+  similar = []
+  for node in g.Nodes():
+    union = set()
+    for n in target.GetOutEdges():
+      union.add(n)
+    for n in node.GetOutEdges():
+      union.add(n)
 
-folded_graph = fold_graph()
+    Nbrs = snap.TIntV()
+    snap.GetCmnNbrs(g, node_id, node.GetId(), Nbrs)
+    similar.append((float(Nbrs.Len()) / float(len(union)), node_id_to_info[node.GetId()]))
 
-print 'Nodes: ' + str(folded_graph.GetNodes())
-print 'Edges: ' + str(folded_graph.GetEdges())
+  return sorted(similar, reverse = True)[0:6]
+
+# fold_graph()
+FIn = snap.TFIn(folded_grph_str)
+folded_graph = snap.TUNGraph.Load(FIn)
+
+# t5_2cneq - politics subreddit - ~100 comments in the subset
+
+print map(lambda info: info[1]['name'], get_jaccard(folded_graph, info_to_node_id['t5_2cneq']))
